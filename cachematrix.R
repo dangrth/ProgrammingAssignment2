e21a369a5b2c82675291109c6e922ebebb0e5dff
Thibault Dangreaux <thibault.dangreaux@gmail.com>
2015-01-22 17:02:30 +0100

    Assignment done
    Assignment done, sharing to GitHub

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Manages matrices with a cached inversion result
 
-## Write a short comment describing this function
+## Factory to build matrices with caching mechanism included
 
 makeCacheMatrix <- function(x = matrix()) {
 
+  inverse <<- NULL
+  
+  set <- function(y) {
+    x <<- y
+    inverse <<- NULL
+  }
+  
+  get <- function() x
+  
+  setInverse <- function(i) inverse <<- i
+  
+  getInverse <- function() inverse
+  
+  list(set=set, get=get, setInverse=setInverse, getInverse=getInverse)
 }
 
 
-## Write a short comment describing this function
+## Given a matrix build with makeCacheMatrix, returns the inverse
+## from the cache or calculate and cache it if it's not already there
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  i <- x$getInverse()
+  if (!is.null(i)) {
+    # the inverse was found in the cache
+    message("Fetching inverse from cache")
+    return (i)
+  }
+  # No inverse in cache : we need to calculate it
+  matrix <- x$get()
+  # For this assignment, assume that the matrix supplied is always invertible.
+  # No need to check for errors
+  inverse <- solve(matrix)
+  x$setInverse(inverse)
+  inverse
 }
